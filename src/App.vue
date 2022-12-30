116b1e059d5b74df64ccf4c98489b977304f11c7
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2022-12-30 14:01:17 +0300

    Start project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Количество лайков: <strong>{{ likes }}</strong>
+  </div>
+  <div>
+    Количество дизлайков: <strong>{{ dislikes }}</strong>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    };
+  },
+};
+</script>
+
+<style></style>

6d5356c19c15729652571877b51eca3d63a6f691
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2022-12-30 17:19:53 +0300

    Двустороннее связывание. Создание нового поста. Общие стили

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="post" v-for="post in posts" v-bind:key="post.id">
-    <div><strong>Название: </strong>{{ post.title }}</div>
-    <div><strong>Описание: </strong>{{ post.body }}</div>
+  <div class="wrap">
+    <!-- Модификатор @submit.prevent эквивалентен event.preventDefault() -->
+    <form class="form" @submit.prevent>
+      <h4>Создание поста</h4>
+      <input class="input" type="text" placeholder="Название поста" v-bind:value="title" @input="inputTitle" />
+      <!-- Директива v-bind:value="title" связывает инпут с моделью title из data. -->
+      <!-- Директива @input="inputTitle" связывает значение инпута с функцией обновления, таким образом реализовано двустороннее связывание -->
+      <input class="input" type="text" placeholder="Описание поста" v-bind:value="body" @input="inputBody" />
+      <button type="submit" class="form_button" @click="createPost">Отправить</button>
+    </form>
+    <div class="post" v-for="post in posts" v-bind:key="post.id">
+      <!-- Директива v-for="post in posts" позволяет нам отрендерить элементы на основе массива
+		Директива v-bind:key="post.id" создает для каждого элемента свой ключ -->
+      <div><strong>Название: </strong>{{ post.title }}</div>
+      <div><strong>Описание: </strong>{{ post.body }}</div>
+    </div>
   </div>
 </template>
 
@@ -12,20 +25,45 @@ export default {
       likes: 0,
       dislikes: 5,
       posts: [
-        { id: 1, title: 'Информация о JavaScript1', body: 'Описание поста1' },
-        { id: 2, title: 'Информация о JavaScript2', body: 'Описание поста2' },
-        { id: 3, title: 'Информация о JavaScript3', body: 'Описание поста3' },
-        { id: 4, title: 'Информация о JavaScript4', body: 'Описание поста4' },
-        { id: 5, title: 'Информация о JavaScript5', body: 'Описание поста5' },
+        { id: 1, title: 'Информация о JavaScript 1', body: 'Описание поста 1' },
+        { id: 2, title: 'Информация о JavaScript 2', body: 'Описание поста 2' },
+        { id: 3, title: 'Информация о JavaScript 3', body: 'Описание поста 3' },
+        { id: 4, title: 'Информация о JavaScript 4', body: 'Описание поста 4' },
+        { id: 5, title: 'Информация о JavaScript 5', body: 'Описание поста 5' },
       ],
+      title: '',
+      body: '',
     };
   },
   methods: {
     addLike() {
       this.likes += 1;
     },
+
     addDisLike() {
       this.dislikes += 1;
+    },
+
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+
+      //Добавление в общий массив постов
+      this.posts.push(newPost);
+
+      this.title = '';
+      this.body = '';
+    },
+
+    inputTitle(event: any) {
+      this.title = event.target.value;
+    },
+
+    inputBody(event: any) {
+      this.body = event.target.value;
     },
   },
 };
@@ -43,5 +81,38 @@ export default {
   padding: 15px;
   border: 2px solid teal;
   margin: 15px 15px 0;
+}
+
+.wrap {
+  margin: 15px 15px;
+  border: 2px solid #c1c1c1;
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
+
+.form {
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
+.input {
+  width: 100%;
+  border: 2px solid rgb(6, 223, 219);
+  padding: 10px 15px;
+  margin-top: 10px;
+}
+
+.input:first-child {
+  margin-top: 0;
+}
+
+.form_button {
+  margin-top: 15px;
+  margin-bottom: 30px;
+  padding: 10px;
+  background: none;
+  border: 2px solid rgb(6, 223, 219);
+
+  width: 100px;
 }
 </style>
